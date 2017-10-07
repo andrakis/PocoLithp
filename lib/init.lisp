6172fa18176083bf4e7a11236f9943fec0980a04
Julian Thatcher <daedalus.raistlin@gmail.com>
2017-10-07 21:22:58 +1100

    Terrible hack required on or1k :(

--- lib/init.lisp
+++ lib/init.lisp
@@ -1,7 +1,5 @@
 ;; Entry point for standard REPL environment
 (begin
-	(define DefaultLibraries (list lists numeric string misc))
-
 	;; (timing true)
 	;; (debug true)
 	(define __module__ (macro () init))
@@ -9,25 +7,16 @@
 	(define __require_debug (# (Message) (begin
 		(if (= true (get! __init_debug)) (print Message))
 	)))
-	(define import (# (Path) (begin
-		(define Content (readfile (+ "lib/" (str Path) ".lisp")))
-		(if (> (length Content) 0)
-			(begin
-				;; Got file contents, can import
-				;; Run in topmost context
-				(_eval_ctx (_tokenize Content))
-			)
-			;; else
-			(begin
-				;; Failed to get contents, throw error
-				(invalid_arg (+ "Unable to import module: " (str Path)))
-			)
-		)
-	)))
+	(define import (# (Path)
+		;; Run in topmost context
+		(_eval_ctx (_tokenize (readfile (+ "lib/" (str Path) ".lisp"))))
+	))
 	(import "require")
 
 	;; Import default libraries
-	(each DefaultLibraries (# (Lib) (require Lib)))
+	(require numeric)
+	(require string)
+	(require misc)
 
 	(print (banner))
 	(repl)
